71c081459a1d31c0092dff4751a2cffd776abeeb
firedcto@gmail.com <firedcto@gmail.com>
2024-06-08 16:41:13 +0800

    first commit
    (cherry picked from commit 325f53eee9689a0467db0180ee922891a09713e0)

--- main_debug_console.py
+++ main_debug_console.py
@@ -2,5 +2,8 @@ from config import config
 from ok.OK import OK
 
 config = config
+config['debug'] = True
+config['use_gui'] = False
+config['onetime_tasks'][1].enable()
 ok = OK(config)
 ok.start()
